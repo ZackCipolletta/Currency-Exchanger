cc67ae66701632208bab6085a9627b3e695f71d5
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-10 16:10:32 -0800

    Update error handling to use the custom API messages from the API response.

--- src/js/currencyExchanger.js
+++ src/js/currencyExchanger.js
@@ -1,10 +1,9 @@
 export default class CurrencyExchange {
   static async getExchangeRates() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`) // <-- change 'USD' to another variable to be able to get exchange rates between any two currencies, not just USD to X.
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/e15c41ceb3769636ce569121/codes`); // <-- change 'USD' to another variable to be able to get exchange rates between any two currencies, not just USD to X.
       // to conver between any two specific currencies use: https://v6.exchangerate-api.com/v6/${API_KEY}/pair/USD/GBP.  change USD and GBP to variables which will be gathered from user input to exchange between any two variables.
 
-      // use https://v6.exchangerate-api.com/v6/e15c41ceb3769636ce569121/codes to get all supported currencies and corresponding codes.
       const jsonifiedResponse = await response.json();
       if (!response.result === 'success') {
         const errorMessage = `${response.result} ${response['error-type']}`;
@@ -16,20 +15,20 @@ export default class CurrencyExchange {
     }
   }
   static async getExchangeAmount() {
-    try { const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    const jsonifiedResponse = await response.json();
-    if (!response.ok) {
-      const errorMessage = `${response.status} ${response.result} ${response['error-type']}`;
-      throw new Error(errorMessage);
-    }
-    return jsonifiedResponse;
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/e15c41ceb3769636ce569121/latest/USD`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        let errorMessage = `${response.status}. Reason:${response.statusText} ${jsonifiedResponse['error-type']}.`;
+        if (jsonifiedResponse['extra-info']) {
+          console.log('this branch is firing')
+          errorMessage = errorMessage.concat(` ${jsonifiedResponse['extra-info']}`)
+        }
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
     } catch (error) {
       return error;
     }
   }
 }
-
-
-// codes.supported_codes.forEach(element => {
-//   console.log(element[0]);
-// });
