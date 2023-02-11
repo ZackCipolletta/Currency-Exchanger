eb47a36ce92e4605ee9dbb77ac076f1061669c9f
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-02-10 18:05:02 -0800

    add ability to store API response in storageSession

--- src/js/currencyExchanger.js
+++ src/js/currencyExchanger.js
@@ -1,9 +1,7 @@
 export default class CurrencyExchange {
-  static async getExchangeRates() {
+  static async getSupportedCurrencies() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/e15c41ceb3769636ce569121/codes`); // <-- change 'USD' to another variable to be able to get exchange rates between any two currencies, not just USD to X.
-      // to conver between any two specific currencies use: https://v6.exchangerate-api.com/v6/${API_KEY}/pair/USD/GBP.  change USD and GBP to variables which will be gathered from user input to exchange between any two variables.
-
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
       const jsonifiedResponse = await response.json();
       if (!response.result === 'success') {
         const errorMessage = `${response.result} ${response['error-type']}`;
